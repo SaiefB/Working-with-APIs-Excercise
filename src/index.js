39d63955ab11825026dc3cc18c36e5349ec21fd7
Saief <A.S.Bokor@outlook.com>
2025-12-12 18:37:05 +0000

    complete excercise, ready for assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import "./styles.css";
 
-const unusedVariable = "This variable is not used anywhere in the code";
+const img = document.querySelector("img");
+
+fetch(
+  "https://api.giphy.com/v1/gifs/translate?api_key=GUPIKph3Ma7PhSBobqWJzO56PaWgQmir&s=cats",
+)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    img.src = response.data.images.original.url;
+  });
